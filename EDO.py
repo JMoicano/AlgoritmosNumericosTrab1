e44cd8fe7b25daa7ace35e96af6e000eb84155b0
JMoicano <jmariossilva@gmail.com>
2017-06-28 15:03:15 -0300

    Now returning matrix b too

--- EDO.py
+++ EDO.py
@@ -9,6 +9,25 @@ def getLine(p, q, h, x):
 	line.append((1/h**2) + p(x)/2*h)
 	return line
 
-def getMatrix(a, b, m, p, q):
+def getMatrix(a, b, m, p, q, r, ya, yb):
 	x, h = getXH(a, b, m)
-	return [getLine(p, q, h, xi) for xi in x[1:-1]]
+	
+	MatA = [getLine(p, q, h, xi) for xi in x[2:-2]]
+	
+	firstLine = []
+	firstLine.append((-2/h**2) - q(x[1]))
+	firstLine.append((1/h**2) + p(x[1])/2*h)
+	MatA.insert(0, firstLine)
+	
+	lastLine = []
+	lastLine.append((1/h**2) - p(x[m-1])/2*h)
+	lastLine.append((-2/h**2) - q(x[m-1]))
+	MatA.append(lastLine)
+
+	MatB = [r(xi) for xi in x[2:-2]]
+
+	MatB.insert(0, r(x[1])*ya - (1/h**2) - p(x[1])/2*h)
+
+	MatB.append(r(x[m-1])*yb - (1/h**2) + p(x)/2*h)
+
+	return MatA, MatB
